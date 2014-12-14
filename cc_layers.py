2dc46d6fcf691baf7b93745f5cfa8837ac88052b
Petros Ypsilantis <pet_yps@hotmail.com>
2014-12-14 20:18:45 +0000

    Update cc_layers.py

--- cc_layers.py
+++ cc_layers.py
@@ -3,4 +3,9 @@ Layers using cuda-convnet Theano wrappers that are part of pylearn2.
 """
 
 
-import 
+import theano
+import theano.tensor as T
+import numpy as np
+
+from theano.sandbox.cuda.basic_ops import gpu_contiguous
+from pylearn2.sandbox.cuda_convnet.filter_acts import FilterActs
