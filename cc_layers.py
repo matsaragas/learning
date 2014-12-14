356089b0c05c5d3b899052fb2d3dd4ca08bb04c0
Petros Ypsilantis <pet_yps@hotmail.com>
2014-12-14 20:40:03 +0000

    Update cc_layers.py

--- cc_layers.py
+++ cc_layers.py
@@ -9,3 +9,11 @@ import numpy as np
 
 from theano.sandbox.cuda.basic_ops import gpu_contiguous
 from pylearn2.sandbox.cuda_convnet.filter_acts import FilterActs
+from pylearn2.sandbox.cuda_convnet.pool import MaxPool
+from pylearn2.sandbox.cuda_convnet.stochastic_pool import StochasticMaxPool, WeightedMaxPool
+from pylearn2.sandbox.cuda_convnet.response_norm import CrossMapNorm
+from theano.sandbox.cuda import host_from_gpu
+
+
+class CudaConvnetInput2DLayer(layers.Input2DLayer):
+  
